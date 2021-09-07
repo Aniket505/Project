3a9dd04827cbe81da610aabac61a1bc563427a69
Aniket Thakur <55542790+Aniket505@users.noreply.github.com>
2021-09-07 11:52:05 +0530

    header file

---
+++ sort.h
@@ -0,0 +1,10 @@
+void q_sort(void **, int, int , int (*)(void *, void *));
+
+void swap(void **, int, int);
+
+void merge_sort(void **, int , int , void **, int (*)(void *, void *));
+
+void bubble_sort(void ** , int, int (*)(void *, void *));
+
+void insertion_sort(void **, int, int(*)(void *, void *));
+
