27d67252b0fe3e5c39c987160cc517bc091fa519
Aniket Thakur <55542790+Aniket505@users.noreply.github.com>
2021-09-07 11:50:58 +0530

    quick sort

---
+++ qsort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"sort.h"
+void q_sort(void **a, int L, int R, int (*cmp)(void *, void *))
+{
+    if (L >= R)
+    {
+        return;
+    }
+    int last = L;
+    int i = L + 1;
+    for (; i <= R; i++)
+    {
+        if ((*cmp)(a[i], a[L]) < 0)
+        {
+            swap(a, i, ++last);
+        }
+    }
+    swap(a, L, last);
+    q_sort(a, L, last - 1, cmp);
+    q_sort(a, last + 1, R, cmp);
+}
+
